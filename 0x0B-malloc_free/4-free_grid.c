f6d1bd26137c63cdf923130530dc49cc1f481545
4orops <wabile4@gmail.com>
2023-10-04 08:01:07 -0700

    malloc and free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * free_grid - 2 dimension clean up
+ * @grid: grid
+ * @height: height of a dimension
+ * return: nothing
+ * */
+
 void free_grid(int **grid, int height)
 {
 	int i;
